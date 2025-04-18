770750b16b0b47774b01431cc243f3a73aba75b2
FurWhisk <187447641+FurWhisk@users.noreply.github.com>
2025-04-18 06:10:07 -0400

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const App = () => (
-  <div style={{ textAlign: 'center', paddingTop: '50px', fontFamily: 'sans-serif' }}>
-    <h1>FurWhisk Swap – Powered by Whiskers</h1>
-    <p>Coming Soon: WalletConnect, Jupiter Swap, and Referral XP!</p>
-  </div>
-);
-
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
